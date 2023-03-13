0ce7108344e9074040f03c9c7bbd8e7d39968744
yi-fan-song <yfsong00@gmail.com>
2023-03-13 07:07:42 -0400

    near actions and leave

--- src/web3/near/index.ts
+++ src/web3/near/index.ts
@@ -1,6 +1,8 @@
 import BN from "bn.js";
 import { Account, Connection } from "near-api-js";
 
+import { Action, BrowserWalletBehaviour } from "@near-wallet-selector/core";
+
 import { Challenge as ChallengeType } from "../types";
 
 export type Cluster = "MAINNET" | "DEVNET";
@@ -8,6 +10,81 @@ export type Cluster = "MAINNET" | "DEVNET";
 type ConnectionOptions = {
   cluster: Cluster;
 };
+
+type SignAndSendTransaction = BrowserWalletBehaviour["signAndSendTransaction"];
+
+const makeSendTransaction = (receiverId: string, actions: Action[]) => {
+  return async (signAndSendTransaction: SignAndSendTransaction) => {
+    return await signAndSendTransaction({
+      receiverId: receiverId,
+      actions: actions,
+    });
+  };
+};
+
+class ActionsBuilder {
+  private challengePromise: Promise<ChallengeType>;
+  private cluster: Cluster;
+
+  private challengeAddress: number;
+
+  private nearContractId: string;
+  private ludexContractId: string;
+
+  constructor(
+    challengeAddress: number,
+    challengePromise: Promise<ChallengeType>,
+    options?: ConnectionOptions
+  ) {
+    this.challengeAddress = challengeAddress;
+    this.challengePromise = challengePromise;
+    this.cluster = options?.cluster || "MAINNET";
+
+    this.nearContractId =
+      this.cluster === "MAINNET" ? "wrap.near" : "wrap.testnet";
+    this.ludexContractId =
+      this.cluster === "MAINNET" ? "ftc.ludexgg.near" : "ftc.ludex.testnet";
+  }
+
+  public async join() {
+    const challenge = await this.challengePromise;
+
+    const action: Action = {
+      type: "FunctionCall",
+      params: {
+        methodName: "ft_transfer_call",
+        args: {
+          receiver_id: this.ludexContractId,
+          amount: challenge.entryFee.toString(),
+          memo: null,
+          msg: this.challengeAddress.toString(),
+        },
+        gas: "300000000000000",
+        deposit: "1",
+      },
+    };
+
+    const sendTransaction = makeSendTransaction(this.nearContractId, [action]);
+
+    return [action, sendTransaction];
+  }
+
+  public async leave(_account: Account) {
+    const action: Action = {
+      type: "FunctionCall",
+      params: {
+        methodName: "leave_challenge",
+        args: { challenge_id: this.challengeAddress },
+        gas: "300000000000000",
+        deposit: "1",
+      },
+    };
+
+    const sendTransaction = makeSendTransaction(this.ludexContractId, [action]);
+
+    return [action, sendTransaction];
+  }
+}
 
 export class Challenge {
   private _challenge: ChallengeType | undefined;
@@ -34,6 +111,12 @@ export class Challenge {
       this.cluster === "MAINNET" ? "wrap.near" : "wrap.testnet";
     this.ludexContractId =
       this.cluster === "MAINNET" ? "ftc.ludexgg.near" : "ftc.ludex.testnet";
+  }
+
+  public get actions() {
+    return new ActionsBuilder(this.challengeAddress, this.getChallenge(), {
+      cluster: this.cluster,
+    });
   }
 
   public async getChallenge() {
@@ -86,8 +169,8 @@ export class Challenge {
         memo: null,
         msg: this.challengeAddress.toString(),
       },
-      gas: "300000000000000" as any,
-      attachedDeposit: "1" as any,
+      gas: new BN("300000000000000"),
+      attachedDeposit: new BN("1"),
     });
 
     res.receipts_outcome.forEach((receipt) => {
@@ -100,26 +183,30 @@ export class Challenge {
         throw err;
       }
     });
+
+    return res;
   }
 
-  public async leave(_account: Account) {}
+  public async leave(account: Account) {
+    const res = await account.functionCall({
+      contractId: this.ludexContractId,
+      methodName: "leave_challenge",
+      args: { challenge_id: this.challengeAddress },
+      gas: new BN("300000000000000"),
+      attachedDeposit: new BN("1"),
+    });
+
+    res.receipts_outcome.forEach((receipt) => {
+      if (
+        receipt.outcome.status === "Failure" ||
+        receipt.outcome.status.hasOwnProperty("Failure")
+      ) {
+        const err = new Error("Failed to join challenge");
+        Object.assign(err, "receipt", receipt);
+        throw err;
+      }
+    });
+
+    return res;
+  }
 }
-
-// export const joinChallenge = async (connection, account) => {
-//   const exe = await account.functionCall({
-//     contractId: isMainnet ? "wrap.near" : "wrap.testnet",
-//     methodName: "ft_transfer_call",
-//     args: {
-//       receiver_id: isMainnet ? "ftc.ludexgg.near" : "ftc.ludex.testnet",
-//       amount: challenge?.entryAmount + "",
-//       memo: null,
-//       msg: challengeAddress,
-//     },
-//     gas: "300000000000000" as any,
-//     attachedDeposit: "1" as any,
-//   });
-// };
-
-// export const leaveChallenge = async () => {};
-
-// export const redeemPrize = async () => {};
