547ca886da389bd93734cb7e31dfc9a7464df8cd
yi-fan-song <yfsong00@gmail.com>
2023-07-03 10:22:05 -0400

    evm abi build

---
+++ evm/index.ts
@@ -0,0 +1,2 @@
+export { Ludex__factory as LudexContractFactory } from "./generated/factories/Ludex__factory";
+export { FtChallengePool__factory as FtChallengePoolFactory } from "./generated/factories/FtChallengePool__factory";
