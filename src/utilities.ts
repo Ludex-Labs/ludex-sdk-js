0fab4165c910685bf04cf7bea00dc168e51d4c89
allentsangdev <allentsang04@gmail.com>
2024-02-13 16:03:56 -0500

    update avax handling

--- src/utilities.ts
+++ src/utilities.ts
@@ -6,14 +6,14 @@ type parseTransactionResponse = Object | Transaction;
 
 /**
  * Parses a transaction encoded in base64 based on the specified blockchain chain.
- * It returns a generic object to represent a transaction for Avalanche transactions and a
+ * It returns an generic objects to represent a transaction for Avalanche transactions and a
  * Solana Transaction object for Solana transactions.
  *
  * @param {String} txn - The transaction transaction information encoded in base64.
  * @param {Chain} chain - The blockchain chain identifier (AVALANCHE or SOLANA) determining the parsing
  *        method and format of the returned object.
  * @returns {Promise<Object | Transaction>} A Promise that resolves to a parsed transaction, which is
- *          either a JSON object for Avalanche or a Solana Transaction object for Solana.
+ *          either an JSON object for Avalanche or a Solana Transaction object for Solana.
  */
 export const parseTransaction = async (
   txn: String,
@@ -23,7 +23,7 @@ export const parseTransaction = async (
     case Chain.AVALANCHE:
       return JSON.parse(
         Buffer.from(txn, "base64").toString("utf-8")
-      );
+      )[0];
     case Chain.SOLANA:
       return Transaction.from(Buffer.from(txn, "base64"));
   }
