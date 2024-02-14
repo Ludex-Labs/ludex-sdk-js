e26ee51156b004b459cab905f49a395f06a74645
allentsangdev <allentsang04@gmail.com>
2024-02-14 14:56:52 -0500

    add jsDoc for return type

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,11 +1,27 @@
 import { Chain } from "./types";
 
-type parseTransactionResponse = Object | Buffer;
-
 /**
  * Parses a transaction encoded in base64 based on the specified blockchain chain.
  * It returns an generic objects to represent a transaction for Avalanche transactions and a
  * buffer for Solana transactions.
+ * 
+ * For Avalanche, it returns a JSON object representing the transaction, suitable for
+ * libraries like ethers.js. Example usage:
+ * ```
+ * const transaction = await parseTransaction(txn, Chain.AVALANCHE);
+ * const ethersProvider = new ethers.Provider(provider);
+ * const signer = await ethersProvider.getSigner();
+ * const transactionResponse = await signer.sendTransaction(transaction);
+ * ```
+ *
+ * For Solana, it returns a Buffer, which can be used with solana/web3.js and other Solana
+ * libraries. Example usage:
+ * ```
+ * const transactionBuffer = await parseTransaction(txn, Chain.SOLANA);
+ * const solWeb3 = new SolanaWallet(provider);
+ * const transaction = Transaction.from(transactionBuffer);
+ * const response = await solWeb3.signAndSendTransaction(transaction);
+ * ```
  *
  * @param {String} txn - The transaction transaction information encoded in base64.
  * @param {Chain} chain - The blockchain chain identifier (AVALANCHE or SOLANA) determining the parsing
@@ -16,12 +32,10 @@ type parseTransactionResponse = Object | Buffer;
 export const parseTransaction = async (
   txn: String,
   chain: Chain
-): Promise<parseTransactionResponse> => {
+): Promise<Object | Buffer> => {
   switch (chain) {
     case Chain.AVALANCHE:
-      return JSON.parse(
-        Buffer.from(txn, "base64").toString("utf-8")
-      )[0];
+      return JSON.parse(Buffer.from(txn, "base64").toString("utf-8"))[0];
     case Chain.SOLANA:
       return Buffer.from(txn, "base64");
   }
